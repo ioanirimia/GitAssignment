d1ad7cc05c1584e01fa1f892ec7614b5fac8007f
psych <psych@DESKTOP-8F5MQ0P>
2020-03-23 15:13:43 +0200

    Commit 2. Am adaugat o noua clasa cu cele 2 atribute

---
+++ TemaGit/src/Descriere.java
@@ -0,0 +1,13 @@
+
+public class Descriere {
+
+	private int numarStatii;
+	private String Cartier;
+	
+	
+	public Descriere(int numarStatii, String cartier) {
+		super();
+		this.numarStatii = numarStatii;
+		Cartier = cartier;
+	}
+}
