7c8b2625dd5faaf22f239ed9574af601fb46bde4
psych <psych@DESKTOP-8F5MQ0P>
2020-03-23 14:50:18 +0200

    Commit 1

--- TemaGit/src/TemaGit.java
+++ TemaGit/src/TemaGit.java
@@ -1,7 +1,7 @@
 public class TemaGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git! Aplicatie destinata turismului in Bucuresti");
+		System.out.println("Hello Git! Aplicatie destinata turismului in Bucuresti ");
 	}
 
 }
